56bfa9682f88236a969781b78a961a728d42e27c
Mustafa-alkeyam <falahmustafa751@gmail.com>
2023-10-04 21:23:54 +0300

    add h

--- js/index.js
+++ js/index.js
@@ -24,8 +24,35 @@ Rutaceae.addEventListener("click", function() {
     ForestId.style.display = "none";
 });
 
+
 Forest.addEventListener("click", function() {
     RosaceaeId.style.display = "none";
     RutaceaeId.style.display = "none";
     ForestId.style.display = "block";
 });
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+//     addToCartButtons.forEach(button => {
+//       button.addEventListener("click", function() {
+//         const card = this.closest(".card");
+//         const imgSrc = card.querySelector(".card-img-top").getAttribute("src");
+//         const title = card.querySelector(".card-title").textContent;
+//         const text = card.querySelector(".card-text").textContent;
+
+//         const item = {
+//           img: imgSrc,
+//           title: title,
+//           text: text
+//         };
+
+//         // Add the 'item' object to your shopping cart or perform other actions here
+//         console.log("Added to cart:", item);
+//       });
+//     });
+//   });
